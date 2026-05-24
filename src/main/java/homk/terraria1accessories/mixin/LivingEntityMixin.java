73502e59ba15f77cc6b1108502f599d7ee1a8a49
homk123 <dima123to@mail.ru>
2026-05-24 18:53:20 +0300

    blablabla

--- src/main/java/homk/terraria1accessories/mixin/LivingEntityMixin.java
+++
@@ -1,8 +0,0 @@
-package homk.terraria1accessories.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.world.entity.LivingEntity;
-
-@Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin {
-}
